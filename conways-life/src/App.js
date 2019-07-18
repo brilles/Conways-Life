7b68d425f7534d2a08c5d90059717f20c6f68402
Brian Illes <brianrilles@gmail.com>
2019-07-18 16:58:05 -0700

    Adds about content

--- conways-life/src/App.js
+++ conways-life/src/App.js
@@ -3,23 +3,29 @@ import "./styles/App.css";
 import Rules from "./components/Rules";
 import Game from "./components/Game";
 import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Conway's Game of Life</h1>
-      <div className="main">
-        <div className="game-wrapper">
-          <Game />
+    <>
+      <div className="App">
+        <h1>Conway's Game of Life</h1>
+        <div className="main">
+          <div className="game-wrapper">
+            <Game />
+          </div>
+          <div className="rules-wrapper">
+            <Rules />
+          </div>
         </div>
-        <div className="rules-wrapper">
-          <Rules />
+        <div className="about-wrapper">
+          <About />
         </div>
       </div>
-      <div className="about-wrapper">
-        <About />
+      <div className="footer-wrapper">
+        <Footer />
       </div>
-    </div>
+    </>
   );
 }
 
--- conways-life/src/components/About.js
+++ conways-life/src/components/About.js
@@ -5,12 +5,19 @@ export default function About() {
   return (
     <div className="about">
       <h2>About</h2>
+      <h3>Is the Game of Life Turing Complete?</h3>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi suscipit
-        tellus vel consequat cursus. Fusce ut dignissim risus. Cras libero
-        libero, pretium eget lacus aliquet, gravida faucibus tortor. Ut id sem
-        nisl. Mauris imperdiet diam quis porta laoreet. Integer sodales vel mi
-        sit amet venenatis.
+        Something is said to be Turing complete if it is capable of computing
+        that which a Turing Machine can compute. A Turing Machine is a
+        theoretical mechanical calculator that can run calculations. It has a
+        tape (instruction set) that is divided into cells. These cells can have
+        values 0 or 1 which can be read from or written to as the machine moves
+        along the tape. It can do arithmetic conditional branching, and store
+        data. With that, anything that is computable can be calculated.
+      </p>
+      <p className="bottom">
+        The Game of Life can compute anything that is computable given a enought
+        time and a large enough grid therefore it is Turing Complete.
       </p>
     </div>
   );
