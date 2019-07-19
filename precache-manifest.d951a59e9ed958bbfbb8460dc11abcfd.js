568660c47e0251aa5b8b198528cc83d4b29cfdec
Brian Illes <brianrilles@gmail.com>
2019-07-18 21:20:42 -0700

    Updates

--- precache-manifest.d951a59e9ed958bbfbb8460dc11abcfd.js
+++ precache-manifest.d951a59e9ed958bbfbb8460dc11abcfd.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "271d82b8eeb657ce1e37869eed5733e3",
+    "revision": "edee8152a9f350b157cdcf81fdcc67d9",
     "url": "/Conways-Life/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Conways-Life/static/css/2.d3a7c30d.chunk.css"
   },
   {
-    "revision": "e39a15b2b9e58dc6f9a9",
+    "revision": "60804423194a68e87626",
     "url": "/Conways-Life/static/css/main.9ca2cf4f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Conways-Life/static/js/2.526306c2.chunk.js"
   },
   {
-    "revision": "e39a15b2b9e58dc6f9a9",
-    "url": "/Conways-Life/static/js/main.52da3a12.chunk.js"
+    "revision": "60804423194a68e87626",
+    "url": "/Conways-Life/static/js/main.46ff5ea6.chunk.js"
   },
   {
     "revision": "f94638f27adfa8c548ad",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Conways-Life/precache-manifest.e0d334fd68448f9b5201991eea9072e0.js"
+  "/Conways-Life/precache-manifest.d951a59e9ed958bbfbb8460dc11abcfd.js"
 );
 
 self.addEventListener('message', (event) => {
